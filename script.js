90ef21b0547727aa21a0ac8f02947125eb17382e
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-08-02 18:34:13 +0300

    add animation move to new page and refactor code

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-// Логика прилоадера
+// Логика прелоадера
 document.addEventListener('DOMContentLoaded', () => {
-    const mediaFiles = document.querySelectorAll('img, video');
-    let i = 0
-    Array.from(mediaFiles).forEach((file, index) => {
-        file.onload = () => {
-            i++
-            percents.innerHTML = ((i * 100) / mediaFiles.length).toFixed(1)
-            if(i === mediaFiles.length) {
-                preloader.classList.add('preloader--hide')
-                percents.innerHTML = 100
-            }
-        }
-    })
+  const mediaFiles = document.querySelectorAll('img, video');
+  let i = 0
+
+  Array.from(mediaFiles).forEach((file, index) => {
+    file.onload = () => {
+      i++
+      percents.innerHTML = ((i * 100) / mediaFiles.length).toFixed(1)
+      if(i === mediaFiles.length) {
+        preloader.classList.add('preloader--hide')
+        percents.innerHTML = 100
+      }
+    }
+  })
 })
 
-// когда страница полностью загружена или сохранена в кеше браузера и загружается моментально то убираем прелоадер
-function loadPage() {
-    preloader.classList.add('preloader--hide')
+const loadPage = () => {
+ preloader.classList.add('preloader--hide')
 }
-let posTop = window.pageYOffset;
-document.addEventListener('scroll', function() {
-    console.log(posTop)
-});
 
-function scrollWin() {
-    //document.location.href = "http://www.site.ru"
-    console.log('scroll')
-    window.scrollTo(0, 800);
+
+if (document.addEventListener) {
+  if ('onwheel' in document) {
+    // IE9+, FF17+, Ch31+
+    document.addEventListener("wheel", onWheel);
+  } else if ('onmousewheel' in document) {
+    // устаревший вариант события
+    document.addEventListener("mousewheel", onWheel);
+  } else {
+    // Firefox < 17
+    document.addEventListener("MozMousePixelScroll", onWheel);
+  }
+} else { // IE8-
+  document.attachEvent("onmousewheel", onWheel);
 }
+
+function onWheel(e) {
+  e = e || window.event;
+  var delta = e.deltaY || e.detail || e.wheelDelta;
+
+  if(delta> 0){
+    hole.classList.add('_active')
+  }else{
+    hole.classList.remove('_active')
+  }
+  
+  e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+}
+
+const move_button = document.getElementById('move_button');
+const whiteBoard = document.getElementById('white-board');
+
+move_button.addEventListener('click', function(){
+    whiteBoard.classList.add('_active')
+    setTimeout(()=>{
+        location.href = 'newPage.html';
+    },1000)
+})
